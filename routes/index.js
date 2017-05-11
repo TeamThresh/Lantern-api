ad7616a39c6ab7f099cc165a5bf6c188116ea864
GDmail <GDmail91@gmail.com>
2017-05-11 18:50:27 +0900

    *fix mistake

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ module.exports = function(){
   router.get('/statusOfActivity/:packageName', api.package.getStatusOfActivity);
   router.get('/group/:packageName', api.package.getGroupList);
   router.get('/group/:packageName/:groupName', api.package.getStatusByGroup);
-  router.post('/statusByGroup/:packageName', api.package.setStatusByGroup);
+  router.post('/group/:packageName', api.package.setStatusByGroup);
 
   // Summary View
   router.get('/one-depth-userflow/:packageName/:activityName', api.link.getUserflow);
