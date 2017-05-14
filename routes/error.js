27a43e994ffc57eb5c0db43b9855c117e6d9b34e
GDmail <GDmail91@gmail.com>
2017-05-14 23:05:10 +0900

    *rm logs

--- routes/error.js
+++ routes/error.js
@@ -6,10 +6,7 @@ var errorCode = require('./errorCode');
 module.exports = function (err, res, mode) {
     console.log(err);
     var error;
-    console.log(err.status);
     err.status == undefined ? error = errorCode[500] : error = errorCode[err.status];
-    console.log(errorCode[500]);
-    console.log(error);
     if (mode === 'development') {
         // development error handler
         res.status(error.status).json(error.msg);
