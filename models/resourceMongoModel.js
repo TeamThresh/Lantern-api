3931fc5423c2f4d470c917101a0e445d7f140efb
GDmail <GDmail91@gmail.com>
2017-05-25 02:07:59 +0900

    *fix mongo optimization

--- models/resourceMongoModel.js
+++ models/resourceMongoModel.js
@@ -7,14 +7,6 @@ var filterOption = require('./filterOption');
 // MongoDB module
 var mongoose = require('mongoose');
 
-// CONNECT TO MONGODB SERVER
-var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function(){
-    // CONNECTED TO MONGODB SERVER
-    console.log("Connected to mongod server");
-});
-
 mongoose.connect(credentials.mongodb.host);
 var Schema = mongoose.Schema;
 var SchemaSet = require(credentials.mongoSchemaSet.resourceSchemaSet);
@@ -32,56 +24,55 @@ module.exports.resAppMongoModel = function(data) {
 	    filterOption.addMongoFullOption(data.filter, matchQuery);
 
 		var Res = mongoose.model('resourceModels', resourceSchema);
-		Res.aggregate({
-			    $match : matchQuery
+		Res.aggregate([{
+	    		$match : matchQuery
 			}, {
-			    $unwind : "$data"
-			    
+				$limit : 100
 			}, {
-			    $match : {
-			        "data.type" : "res"
-			    }
+				$unwind : "$data"
 			}, {
-			    $group : {
-			        _id : "$_id",
-			        data : {
-		                $push : {
-		                    "state" : "$data.app.cpu_app.state",
-		                    "ppid" : "$data.app.cpu_app.ppid",
-		                    "pgrp" : "$data.app.cpu_app.pgrp",
-		                    "session" : "$data.app.cpu_app.session",
-		                    "tty_nr" : "$data.app.cpu_app.tty_nr",
-		                    "flags" : "$data.app.cpu_app.flags",
-		                    "mingflt" : "$data.app.cpu_app.mingflt",
-		                    "cminfltmajflt" : "$data.app.cpu_app.cminfltmajflt",
-		                    "cmajflt" : "$data.app.cpu_app.cmajflt",
-		                    "utime" : "$data.app.cpu_app.utime",
-		                    "stime" : "$data.app.cpu_app.stime",
-		                    "cutime" : "$data.app.cpu_app.cutime",
-		                    "cstime" : "$data.app.cpu_app.cstime",
-		                    "priority" : "$data.app.cpu_app.priority",
-		                    "nice" : "$data.app.cpu_app.nice",
-		                    "num_threadsitrealvalue" : "$data.app.cpu_app.num_threadsitrealvalue",
-		                    "starttime" : "$data.app.cpu_app.starttime",
-		                    "vsize" : "$data.app.cpu_app.vsize",
-		                    "rss_" : "$data.app.cpu_app.rss_",
-		                    "rsslim" : "$data.app.cpu_app.rsslim",
-		                    "startcode" : "$data.app.cpu_app.startcode",
-		                    "endcode" : "$data.app.cpu_app.endcode",
-		                    "startstackkstkesp" : "$data.app.cpu_app.startstackkstkesp",
-		                    "kstkeip" : "$data.app.cpu_app.kstkeip",
-		                    "signal" : "$data.app.cpu_app.signal",
-		                    "blocked" : "$data.app.cpu_app.blocked",
-		                    "sigignore" : "$data.app.cpu_app.sigignore",
-		                    "sigcatch" : "$data.app.cpu_app.sigcatch",
-		                    "wchan" : "$data.app.cpu_app.wchan",
-		                    "nswap" : "$data.app.cpu_app.nswap",
-		                    "start_time" : "$data.duration_time.start",
-		                    "end_time" : "$data.duration_time.start"
-		                }
-		            }
-			    }
-			}, function(err, resRawData){
+				$sort : { "data.duration_time.start" : -1 }
+			}, {
+				$group : {
+					_id : "app",
+					data : {
+						$push : {
+							"state" : "$data.app.cpu_app.state",
+							"ppid" : "$data.app.cpu_app.ppid",
+							"pgrp" : "$data.app.cpu_app.pgrp",
+							"session" : "$data.app.cpu_app.session",
+							"tty_nr" : "$data.app.cpu_app.tty_nr",
+							"flags" : "$data.app.cpu_app.flags",
+							"mingflt" : "$data.app.cpu_app.mingflt",
+							"cminfltmajflt" : "$data.app.cpu_app.cminfltmajflt",
+							"cmajflt" : "$data.app.cpu_app.cmajflt",
+							"utime" : "$data.app.cpu_app.utime",
+							"stime" : "$data.app.cpu_app.stime",
+							"cutime" : "$data.app.cpu_app.cutime",
+							"cstime" : "$data.app.cpu_app.cstime",
+							"priority" : "$data.app.cpu_app.priority",
+							"nice" : "$data.app.cpu_app.nice",
+							"num_threadsitrealvalue" : "$data.app.cpu_app.num_threadsitrealvalue",
+							"starttime" : "$data.app.cpu_app.starttime",
+							"vsize" : "$data.app.cpu_app.vsize",
+							"rss_" : "$data.app.cpu_app.rss_",
+							"rsslim" : "$data.app.cpu_app.rsslim",
+							"startcode" : "$data.app.cpu_app.startcode",
+							"endcode" : "$data.app.cpu_app.endcode",
+							"startstackkstkesp" : "$data.app.cpu_app.startstackkstkesp",
+							"kstkeip" : "$data.app.cpu_app.kstkeip",
+							"signal" : "$data.app.cpu_app.signal",
+							"blocked" : "$data.app.cpu_app.blocked",
+							"sigignore" : "$data.app.cpu_app.sigignore",
+							"sigcatch" : "$data.app.cpu_app.sigcatch",
+							"wchan" : "$data.app.cpu_app.wchan",
+							"nswap" : "$data.app.cpu_app.nswap",
+							"start_time" : "$data.duration_time.start",
+							"end_time" : "$data.duration_time.start"
+						}
+					}
+				}
+			}], function(err, resRawData){
 		        if(err) {
 		        	var error = new Error(err);
 		        	error.status = 500;
@@ -102,21 +93,6 @@ module.exports.resAppMongoModel = function(data) {
 		        
 		        return resolved(resData);
 	    });
-		/*Res.find({
-			"package_name" : data.package_name,
-		    "device_info.uuid" : data.uuid,
-		    "data": {
-		        $elemMatch: {
-		            type : "res",
-		            "duration_time.start" : { $gt : data.startRange, $lt : data.endRange },
-		            "duration_time.end" : { $gt : data.startRange, $lt : data.endRange }
-		        }
-		    }}, function(err, resData){
-		        if(err) return rejected(err);
-		        if(!resData) return rejected("No data");
-		        
-		        return resolved(resData);
-	    });*/
 
 	});
 }
@@ -127,27 +103,24 @@ module.exports.resOSMongoModel = function(data) {
 		var matchQuery = {
 	        "package_name" : data.package_name,
 			"data.app.activity_stack" : data.activity_name,
-	        "data.type" : "res",
-	        "data.duration_time.start" : { $gt : data.startRange, $lt : data.endRange },
-	        "data.duration_time.end" : { $gt : data.startRange, $lt : data.endRange }
+	        "data.type" : "res"
 	    }
 
 	    filterOption.addMongoFullOption(data.filter, matchQuery);
 	    
 		var Res = mongoose.model('resourceModels', resourceSchema);
-		Res.aggregate({
+		Res.aggregate([{
 			    $match : matchQuery
 			}, {
-			    $unwind : "$data"
-			    
+				$limit : 100
 			}, {
-			    $match : {
-			        "data.type" : "res"
-			    }
+				$unwind : "$data"
 			}, {
-			    $group : {
-			        _id : "$_id",
-			        data : {
+				$sort : { "data.duration_time.start" : -1 }
+			}, {
+				$group : {
+					_id : "os",
+					data : {
 		                $push : {
 		                    "user" : "$data.os.cpu.user",
 		                    "nice" : "$data.os.cpu.nice",
@@ -164,7 +137,7 @@ module.exports.resOSMongoModel = function(data) {
 		                }
 		            }
 			    }
-			}, function(err, resRawData){
+			}], function(err, resRawData){
 		        if(err) {
 		        	var error = new Error(err);
 		        	error.status = 500;
@@ -196,27 +169,24 @@ module.exports.resVmstatMongoModel = function(data) {
 		var matchQuery = {
 	        "package_name" : data.package_name,
 			"data.app.activity_stack" : data.activity_name,
-	        "data.type" : "res",
-	        "data.duration_time.start" : { $gt : data.startRange, $lt : data.endRange },
-	        "data.duration_time.end" : { $gt : data.startRange, $lt : data.endRange }
+	        "data.type" : "res"
 	    }
 
 	    filterOption.addMongoFullOption(data.filter, matchQuery);
 
 		var Res = mongoose.model('resourceModels', resourceSchema);
-		Res.aggregate({
+		Res.aggregate([{
 			    $match : matchQuery
 			}, {
-			    $unwind : "$data"
-			    
+				$limit : 100
 			}, {
-			    $match : {
-			        "data.type" : "res"
-			    }
+				$unwind : "$data"
 			}, {
-			    $group : {
-			        _id : "$_id",
-			        data : {
+				$sort : { "data.duration_time.start" : -1 }
+			}, {
+				$group : {
+					_id : "vmstat",
+					data : {
 		                $push : {
 		                    "r" : "$data.os.vmstat.r",
 		                    "b" : "$data.os.vmstat.b",
@@ -238,7 +208,7 @@ module.exports.resVmstatMongoModel = function(data) {
 		                }
 		            }
 			    }
-			}, function(err, resRawData){
+			}], function(err, resRawData){
 		        if(err) {
 		        	var error = new Error(err);
 		        	error.status = 500;
@@ -280,16 +250,15 @@ module.exports.resMemoryMongoModel = function(data) {
 		Res.aggregate({
 			    $match : matchQuery
 			}, {
-			    $unwind : "$data"
-			    
+				$limit : 100
 			}, {
-			    $match : {
-			        "data.type" : "res"
-			    }
+				$unwind : "$data"
 			}, {
-			    $group : {
-			        _id : "$_id",
-			        data : {
+				$sort : { "data.duration_time.start" : -1 }
+			}, {
+				$group : {
+					_id : "memory",
+					data : {
 		                $push : {
 		                    "max" : "$data.app.memory.max",
 		                    "total" : "$data.app.memory.total",
