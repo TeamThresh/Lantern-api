767fee3fccef82d6b0884a7bd2a9a15bd569913c
Youngsoo Ryu <gdmail91@gmail.com>
2017-10-05 23:10:39 +0900

    *chg CPU model max raw rate

--- models/cpuModel.js
+++ models/cpuModel.js
@@ -6,9 +6,9 @@ var cpuModel = {
     getCpuUsageList : function(context, data) {
         return new Promise(function(resolved, rejected) {
             var select = [data.act_id_list];
-            var sql = "SELECT cpu_raw_rate, cpu_raw_count, cpu_raw_time " +
-            	"FROM cpu_raw_table " +
-            	"WHERE `craw_act_id` IN (?)";
+            var sql = `SELECT cpu_raw_rate, cpu_raw_count, cpu_raw_time 
+            	FROM cpu_raw_table 
+            	WHERE craw_act_id IN (?)`;
             	
             context.connection.query(sql, select, function (err, rows) {
                 if (err) {
@@ -26,7 +26,7 @@ var cpuModel = {
 	            rows.forEach(function(row) {
 	            	var usageData = {
 	            		timestamp : row.cpu_raw_time,
-	            		value : row.cpu_raw_rate,
+	            		value : row.cpu_raw_rate > 100 ? 100 : row.cpu_raw_rate,
 	            		count : row.cpu_raw_count
 	            	};
 	            	data.cpuUsageList.push(usageData);
